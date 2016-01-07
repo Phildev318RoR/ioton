bd36a80a3b18e86119b61c1789afbfcb25e178d5
Philip <phildev318@gmail.com>
2016-01-07 12:28:26 +0000

    add startup code

--- config/routes.rb
+++ config/routes.rb
@@ -53,4 +53,9 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root 'pages#home'
+
+  get 'pages/home', to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
+
 end
