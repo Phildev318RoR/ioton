3204fc82a34881d1620753f59cbd7f87139a7b2d
Philip <phildev318@gmail.com>
2016-01-12 19:30:49 +0000

    add create

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -1,11 +1,37 @@
 class API::PeopleController < ApplicationController
-  http_basic_authenticate_with :name => "myfinance", :password => "credit123"
-  
+  http_basic_authenticate_with :name => "myIoToN", :password => "ioton123"
+  #skip_before_filter :authenticate_user! # we do not need devise authentication here
+  #before_filter :fetch_people, :except => [:index, :create]
+
   #before_action :authenticate
+
+  def fetch_people
+    @people = People.find(params[:id])
+  end
+
   def index
     @people = People.all
     respond_to do |format|
       format.json { render :json => @people }
     end
   end
+
+  def show
+    @people = People.find(params[:id])
+    respond_to do |format|
+      format.json { render :json => @people }
+    end
+  end
+
+  def create
+    @people = People.new(params[:people])
+    respond_to do |format|
+      if @people.save
+        format.json { render json: @people, status: :created }
+      else
+        format.json { render json: @people.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
