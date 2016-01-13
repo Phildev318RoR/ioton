ffd16ac3b9ec3c245ae99db84062f36fe3ba9d9f
Philip <phildev318@gmail.com>
2016-01-13 16:19:21 +0000

    add create145

--- app/controllers/api/people_controller.rb
+++ app/controllers/api/people_controller.rb
@@ -26,7 +26,7 @@ class API::PeopleController < ApplicationController
   end
 
   def create
-    @people = People.new(params.require(:people)) #params[:people])
+    @people = People.new(params[:person])
 
     respond_to do |format|
       format.json { render :json => @people }
