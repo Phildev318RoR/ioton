10af4d70402b2b230d88c046ce151d922a23cd38
Philip <phildev318@gmail.com>
2016-01-25 10:58:51 +0000

    add amcharts1

--- app/helpers/staticpages_helper.rb
+++ app/helpers/staticpages_helper.rb
@@ -2,4 +2,7 @@ module StaticpagesHelper
   def convert_to_amcharts_json(data_array)
    data_array.to_json.gsub(/\"text\"/, "text").html_safe
   end
+  def convert_to_json(data_array)
+   data_array.to_json.html_safe
+  end
 end
